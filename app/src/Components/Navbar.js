f425ec249d768b96f60d40c4313100885d9ba492
h f <waltermelonson@gmail.com>
2019-08-12 21:43:53 -0700

    running into errors with how router dom redirect is not handling a direction like i would want it

--- app/src/Components/Navbar.js
+++ app/src/Components/Navbar.js
@@ -1,56 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-function Navbar(props) {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <a className="navbar-brand" href=" ">
-          Navbar
-        </a>
+class Navbar extends Component {
+  state = {};
 
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Home <span className="sr-only" />
+  option = event => {
+    event.preventDefault();
+    // alert("this thing is working");
+    return <Redirect to="/login" />;
+  };
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <a className="navbar-brand" href=" ">
+            Navbar
+          </a>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Home <span className="sr-only" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/side">
+                  Side
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/secret">
+                  Secret
+                </a>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <a href="/login">
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0"
+                  onClick={this.option}
+                >
+                  {this.props.status}
+                </button>
               </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/side">
-                Side
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/secret">
-                Secret
-              </a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <a href="/login">
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                onClick={props.option}
-              >
-                {props.status}
-              </button>
-            </a>
-          </form>
-        </div>
-      </nav>
-    </div>
-  );
+            </form>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
