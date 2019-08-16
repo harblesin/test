30ce8c20f624b926c21171961f64960fbd56ce29
h f <waltermelonson@gmail.com>
2019-08-15 23:43:39 -0700

    Added small changes to pages to all are uniform

--- app/src/Pages/Sidepage.js
+++ app/src/Pages/Sidepage.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import Row from "../Components/Row";
 import Col from "../Components/Col";
 import Navbar from "../Components/Navbar";
 
-function NoMatch() {
-  return (
-    <div className="App container">
-      <Navbar />
-      <Row>
-        <Col width="col-12">
-          <div className="jumbotron">
-            <h1 className="display-4">This is a sidepage</h1>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
+class NoMatch extends Component {
+  state = {
+    status: "Sign In"
+  };
+  render() {
+    return (
+      <div>
+        <Navbar status={this.state.status} />
+        <Row>
+          <Col width="col-12">
+            <div className="jumbotron shadow-lg mt-4">
+              This is a thing on the sidepage
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
-
 export default NoMatch;
