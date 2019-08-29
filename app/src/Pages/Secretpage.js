56161a8ee3d846cf4c262c40824b2356aece2080
unknown <waltermelonson@gmail.com>
2019-08-28 21:50:03 -0700

    removed unnecessary lines

--- app/src/Pages/Secretpage.js
+++ app/src/Pages/Secretpage.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Row from "../Components/Row";
 import Col from "../Components/Col";
 import Navbar from "../Components/Navbar";
-import Button from "../Components/Button";
-import { Redirect } from "react-router-dom";
 
 class Homepage extends Component {
   state = {
